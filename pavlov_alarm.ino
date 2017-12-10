8989988e6a12dd3b16793634cbdaa3b2e2baac7d
paramono <alex@paramono.com>
2017-12-10 23:15:49 +0300

    Add NTP time sync

--- pavlov_alarm.ino
+++ pavlov_alarm.ino
@@ -13,53 +13,146 @@
 #endif
 #include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
 
+#include <WiFiUdp.h>
+
+#include <Ticker.h>
+
 #define CONTROL_PIN  D2
 
 //flag for saving data
 bool shouldSaveConfig = false;
 int brightness = 0;
 
+Ticker dimmer;
+Ticker udp_handler;
+
+IPAddress timeServerIP; // time.nist.gov NTP server address
+const char* ntpServerName = "time.nist.gov";
+const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+
+WiFiUDP udp;
+unsigned int localPort = 2390; // local port to listen for UDP packets
+
+
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
-    pinMode(CONTROL_PIN, OUTPUT);
-    // put your setup code here, to run once:
-    Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(CONTROL_PIN, OUTPUT);
+  
+  Serial.begin(9600);
+  
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  Serial.println("WM: Initializing");
+  WiFiManager wifiManager;
+  wifiManager.setAPCallback(configModeCallback);
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+  
+  //reset saved settings
+  //wifiManager.resetSettings();
 
-    //WiFiManager
-    //Local intialization. Once its business is done, there is no need to keep it around
-    Serial.println("Initializing WiFiManager");
-    WiFiManager wifiManager;
-    wifiManager.setAPCallback(configModeCallback);
-    wifiManager.setSaveConfigCallback(saveConfigCallback);
-    //reset saved settings
-    //wifiManager.resetSettings();
-    
-    //set custom ip for portal
-    //wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+  //set custom ip for portal
+  //wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
 
-    //fetches ssid and pass from eeprom and tries to connect
-    //if it does not connect it starts an access point with the specified name
-    //here  "AutoConnectAP"
-    //and goes into a blocking loop awaiting configuration
-    Serial.println("Creating AP");
-    wifiManager.autoConnect("PavlovAP", "doglamp");
-    //or use this for auto generated name ESP + ChipID
-    //wifiManager.autoConnect();
+  //fetches ssid and pass from eeprom and tries to connect
+  //if it does not connect it starts an access point with the specified name
+  //here  "AutoConnectAP"
+  //and goes into a blocking loop awaiting configuration
+  Serial.println("WM: Creating AP");
+  wifiManager.autoConnect("PavlovAP", "doglamp");
+  //or use this for auto generated name ESP + ChipID
+  //wifiManager.autoConnect();
 
-    //if you get here you have connected to the WiFi
-    Serial.println("Connected... yay! :)");
+  //if you get here you have connected to the WiFi
+  Serial.println("WM: Connected to AP");
+
+  //unsigned long unixTime = ntpUnixTime(udp);
+
+  Serial.println("UDP: starting");
+  udp.begin(localPort);
+  Serial.print("Local port: ");
+  Serial.println(udp.localPort());
+
+  dimmer.attach(0.010, adjust_brightness);
+  // udp_handler.attach(10.0, ntpUnixTime);
+}
+
+void adjust_brightness() {
+  brightness = (brightness + 1) % 256;
+  analogWrite(CONTROL_PIN, brightness);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-//    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-//    delay(500);                       // wait for a second
-//    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-//    delay(500);                       // wait for a second
+  if (WiFi.status() != WL_CONNECTED) {
+    return;
+  }
 
-    brightness = (brightness + 1) % 256;
-    analogWrite(CONTROL_PIN, brightness);
-    delay(10);
+  
+  // Serial.printf("Hello!\n");
+  // put your main code here, to run repeatedly:
+  //digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  //delay(5000);                       // wait for a second
+  //digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  //delay(500);                       // wait for a second
+  WiFi.hostByName(ntpServerName, timeServerIP); 
+  Serial.print("NTP: connecting to ");
+  Serial.print(ntpServerName);
+  Serial.print(" at ");
+  Serial.print(timeServerIP);
+  
+  sendNTPpacket(timeServerIP); // send an NTP packet to a time server
+  // wait to see if a reply is available
+  delay(1000);
+  
+  int cb = udp.parsePacket();
+  if (!cb) {
+    Serial.println("no packet yet");
+  }
+  else {
+    Serial.print("packet received, length=");
+    Serial.println(cb);
+    // We've received a packet, read the data from it
+    udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+
+    //the timestamp starts at byte 40 of the received packet and is four bytes,
+    // or two words, long. First, esxtract the two words:
+
+    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+    // combine the four bytes (two words) into a long integer
+    // this is NTP time (seconds since Jan 1 1900):
+    unsigned long secsSince1900 = highWord << 16 | lowWord;
+    Serial.print("Seconds since Jan 1 1900 = " );
+    Serial.println(secsSince1900);
+
+    // now convert NTP time into everyday time:
+    Serial.print("Unix time = ");
+    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+    const unsigned long seventyYears = 2208988800UL;
+    // subtract seventy years:
+    unsigned long epoch = secsSince1900 - seventyYears;
+    // print Unix time:
+    Serial.println(epoch);
+
+
+    // print the hour, minute and second:
+    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
+    Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
+    Serial.print(':');
+    if ( ((epoch % 3600) / 60) < 10 ) {
+      // In the first 10 minutes of each hour, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
+    Serial.print(':');
+    if ( (epoch % 60) < 10 ) {
+      // In the first 10 seconds of each minute, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.println(epoch % 60); // print the second
+  }
+  // wait ten seconds before asking for the time again
+  delay(10000);
 }
 
 void configModeCallback(WiFiManager *myWiFiManager) {
@@ -68,10 +161,122 @@ void configModeCallback(WiFiManager *myWiFiManager) {
   Serial.println(myWiFiManager->getConfigPortalSSID());
 }
 
+// send an NTP request to the time server at the given address
+unsigned long sendNTPpacket(IPAddress& address)
+{
+  Serial.println("sending NTP packet...");
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  udp.beginPacket(address, 123); //NTP requests are to port 123
+  udp.write(packetBuffer, NTP_PACKET_SIZE);
+  udp.endPacket();
+}
+
 
 
 //callback notifying us of the need to save config
 void saveConfigCallback() {
   Serial.println("Should save config");
   shouldSaveConfig = true;
+}
+//
+///*
+//   © Francesco Potortì 2013 - GPLv3 - Revision: 1.13
+//
+//   Send an NTP packet and wait for the response, return the Unix time
+//
+//   To lower the memory footprint, no buffers are allocated for sending
+//   and receiving the NTP packets.  Four bytes of memory are allocated
+//   for transmision, the rest is random garbage collected from the data
+//   memory segment, and the received packet is read one byte at a time.
+//   The Unix time is returned, that is, seconds from 1970-01-01T00:00.
+//*/
+void ntpUnixTime()
+{
+  static int udpInited = udp.begin(123); // open socket on arbitrary port
+
+  const char timeServer[] = "pool.ntp.org";  // NTP server
+
+  // Only the first four bytes of an outgoing NTP packet need to be set
+  // appropriately, the rest can be whatever.
+  const long ntpFirstFourBytes = 0xEC0600E3; // NTP request header
+
+  // Fail if WiFiUdp.begin() could not init a socket
+  if (!udpInited) {
+    Serial.println("NTP: WiFiUdp.begin() could not init a socket");
+    return;
+  }
+
+  // Clear received data from possible stray received packets
+  udp.flush();
+
+  // Send an NTP request
+  Serial.println("NTP: Sending request");
+  if (! (udp.beginPacket(timeServer, 123) // 123 is the NTP port
+         && udp.write((byte *)&ntpFirstFourBytes, 48) == 48
+         && udp.endPacket())) {
+    Serial.println("NTP: Request failed");
+    // return;       // sending request failed
+  }
+
+  Serial.println("NTP: not returned");
+  // Wait for response; check every pollIntv ms up to maxPoll times
+  const int pollIntv = 150;   // poll every this many ms
+  const byte maxPoll = 15;    // poll up to this many times
+  int pktLen;       // received packet length
+  Serial.println("NTP: Waiting for response");
+  for (byte i = 0; i < maxPoll; i++) {
+    if ((pktLen = udp.parsePacket()) == 48) {
+      break;
+    }
+    delay(pollIntv);
+  }
+  if (pktLen != 48) {
+    Serial.println("NTP: No correct packet received");
+    return;
+  }
+
+  Serial.println("NTP: Parsing response");
+
+  // Read and discard the first useless bytes
+  // Set useless to 32 for speed; set to 40 for accuracy.
+  const byte useless = 40;
+  for (byte i = 0; i < useless; ++i) {
+    udp.read();
+  }
+
+  // Read the integer part of sending time
+  unsigned long time = udp.read();  // NTP time
+  for (byte i = 1; i < 4; i++)
+    time = time << 8 | udp.read();
+
+  // Round to the nearest second if we want accuracy
+  // The fractionary part is the next byte divided by 256: if it is
+  // greater than 500ms we round to the next second; we also account
+  // for an assumed network delay of 50ms, and (0.5-0.05)*256=115;
+  // additionally, we account for how much we delayed reading the packet
+  // since its arrival, which we assume on average to be pollIntv/2.
+  time += (udp.read() > 115 - pollIntv / 8);
+
+  // Discard the rest of the packet
+  udp.flush();
+
+  unsigned long unixTime = time - 2208988800ul;
+  Serial.print("Unix Time: ");
+  Serial.println(unixTime);
+  //return time - 2208988800ul;   // convert NTP time to Unix time
 }
